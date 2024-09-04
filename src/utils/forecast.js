83e20e2ca5cc4b5a5adaa0dce31fedde3a0f0313
Christopher Palma <christopherpalma19@192.168.1.5>
2024-09-04 22:35:05 +0800

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.weatherstack.com/forecast?access_key=64451f8d612858b53b11f272c7aa4710&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services', undefined)
+        } else if (body.error) {
+            callback('Unable to find location. Try another search', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out.')
+        }
+    })
+}
+
+module.exports = forecast
+
+
